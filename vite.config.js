472e9c6c167075d0117e03daa1b4183879d99aef
Laura Del valle Bermejo <L.delvallebermejo@gmail.com>
2024-08-26 12:25:27 +0200

    arreglos

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], root: './src', // Asegúrate de que este camino sea correcto
-  build: {
-    outDir: '../dist', // Directorio de salida para los archivos construidos
-  },
+  plugins: [react()], 
+  root: './', 
+
 })
