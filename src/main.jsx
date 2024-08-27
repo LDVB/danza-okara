4d9381e67479f14805e7859a3a011574ac6995e0
Laura Del valle Bermejo <L.delvallebermejo@gmail.com>
2024-08-27 11:13:09 +0200

    main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from './App.jsx'
+import App from '../src/App'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
